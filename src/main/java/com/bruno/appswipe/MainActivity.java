f0eb8e4d3b74c0179b6231a28ff2921beec78d90
Bruno Alves <brunovicentealves.ti@gmail.com>
2019-11-04 20:18:59 -0300

    implementado o swipe

--- src/main/java/com/bruno/appswipe/MainActivity.java
+++ src/main/java/com/bruno/appswipe/MainActivity.java
@@ -1,14 +1,56 @@
 package com.bruno.appswipe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout tela ;
+
+    private TextView tvSwite;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        tvSwite = findViewById(R.id.tvSwipe);
+        tela = findViewById(R.id.tela);
+        tela.setOnTouchListener(new OnSwipeTouchListener(this){
+
+            @Override
+            public void onSwipeBottom() {
+                super.onSwipeBottom();
+                tvSwite.setText("para baixo");
+            }
+
+            @Override
+            public void onSwipeTop() {
+                super.onSwipeTop();
+                tvSwite.setText("para cima");
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+                tvSwite.setText("para esquerda");
+            }
+
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+                tvSwite.setText("para direita");
+            }
+
+
+
+        });
     }
 }
